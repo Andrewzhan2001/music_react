98bdd3575e6b2f4ef012976aa0de1202b7ef4c2b
Tianyi.Zhan <tianyi.zhan@outlook.com>
2022-10-02 10:34:04 +0800

    implement error and loader component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className="flex items-center justify-center w-full">
+    <h1 className="text-2xl font-bold text-white">Something went wrong. Please check your internet connection and try again</h1>
+  </div>
 );
 
 export default Error;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,40 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+const Discover = () => {
+  // the data from query, whether the data is fetching for loading..., get if we have error
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = 'Pop';
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-between w-full mt-4 mb-10 sm:flex-row">
+        <h2 className="text-3xl font-bold text-left text-white">Discover {genreTitle}</h2>
+
+        <select
+          onChange={() => {}}
+          value=""
+          className="p-3 mt-5 text-sm text-gray-300 bg-black rounded-lg outline-none sm:mt-0"
+        >
+          {/* value in option specify the value sent to a server when form submitted */}
+          {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+        </select>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {data?.tracks.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
