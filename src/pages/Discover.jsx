d5a1c6aea946356adcea12316966e77881fb7e29
Tianyi.Zhan <tianyi.zhan@outlook.com>
2022-10-23 09:05:56 +0800

    finish search bar function

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,13 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { useGetSongsByGenreQuery } from '../redux/services/shazamCore';
+import { selectGenreListId } from '../redux/features/playerSlice';
 
 const Discover = () => {
   // the data from query, whether the data is fetching for loading..., get if we have error
-  const { data, isFetching, error } = useGetTopChartsQuery();
-  const genreTitle = 'Pop';
-  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'genre-global-chart-1');
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
+  const dispatch = useDispatch();
   if (isFetching) return <Loader title="Loading songs..." />;
   if (error) return <Error />;
   return (
@@ -16,8 +18,8 @@ const Discover = () => {
         <h2 className="text-3xl font-bold text-left text-white">Discover {genreTitle}</h2>
 
         <select
-          onChange={() => {}}
-          value=""
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || 'pop'}
           className="p-3 mt-5 text-sm text-gray-300 bg-black rounded-lg outline-none sm:mt-0"
         >
           {/* value in option specify the value sent to a server when form submitted */}
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -13,12 +13,14 @@ export const shazamApi = createApi({
   // operations want to perform against your server
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/track' }),
+    getSongsByGenre: builder.query({ query: (genreId) => `/charts/track?listId=${genreId}` }),
     getSongDetails: builder.query({ query: ({ songid }) => `/songs/get-details?key=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/songs/list-recommendations?key=${songid}` }),
     getArtistDetails: builder.query({ query: (artistId) => `/artists/get-details?id=${artistId}` }),
     getArtistTopSongs: builder.query({ query: (artistId) => `/artists/get-top-songs?id=${artistId}` }),
+    getSongsBySearch: builder.query({ query: (term) => `/search?term=${term}` }),
   }),
 });
 
 // use + endpoint name + Query
-export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery, useGetArtistTopSongsQuery } = shazamApi;
+export const { useGetTopChartsQuery,useGetSongsByGenreQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery, useGetArtistTopSongsQuery,useGetSongsBySearchQuery } = shazamApi;
